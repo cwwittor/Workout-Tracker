6b89625a046aa621e4f621e13b3b51fbf3e5dd1a
Christian Wittorp <cwwittor@ncsu.edu>
2021-05-11 23:33:28 -0400

    added comments

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+//Schema for the workout, seperated with a date and exercise
 const WkSchema = new Schema({
     day: {
         type: Date,
--- seeders/seed.js
+++ seeders/seed.js
@@ -6,6 +6,7 @@ mongoose.connect("mongodb://localhost/workout", {
   useFindAndModify: false
 });
 
+//Workout Seed
 let workoutSeed = [
   {
     day: new Date().setDate(new Date().getDate()-10),
